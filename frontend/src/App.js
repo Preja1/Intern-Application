ed64802aefd7dae894164d1f0b96fe2a7a6f6bcb
Preja1 <prejabudhathoki46@gmail.com>
2026-04-10 11:16:56 +0545

    Application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Login from './component/Login';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login></Login>}/>
+    </Routes>
+
+    </BrowserRouter>
   );
 }
 
